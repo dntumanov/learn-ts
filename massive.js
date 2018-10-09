cecfd5b95eafac1f6735c4f623ce1bff67ca714c
den-tampli <dtumanov@sfedu.ru>
2018-10-10 00:53:05 +0300

    Массивы

---
+++ massive.js
@@ -0,0 +1,10 @@
+var numArray1 = [1, 2, 3];
+var numArray2 = [1, 2, 3]; //Здесь указывваем более точно, что у нас массив с типом number
+var strArray1 = ['1', '2', '3'];
+var strArray2 = ['1', '2', '3'];
+var boolArray1 = [true, false];
+var boolArray2 = [true, false];
+var array = [1, 2, 3];
+//Если нужно добавиь элемень другого типа в массив(tuples)
+var array1 = [1, 2, '3'];
+var array2 = [false, 'str', 22];
---
+++ massive.ts
@@ -0,0 +1,20 @@
+let numArray1: number[] = [1, 2, 3];
+let numArray2: Array<number> = [1, 2, 3];//Здесь указывваем более точно, что у нас массив с типом number
+
+let strArray1: string[] = ['1', '2', '3'];
+let strArray2: Array<string> = ['1', '2', '3'];
+
+
+let boolArray1: boolean[]  = [true, false];
+let boolArray2: Array<boolean> = [true, false];
+
+let array: number[] = [1, 2, 3];
+//Если нужно добавиь элемень другого типа в массив(tuples)
+let array1: [number, number, string] = [1, 2, '3'];
+
+let array2: [boolean, string, number] = [false, 'str', 22]
+
+
+
+
+
